e3e53dab437c467f3d9687b3301660bd255fa316
CernohorskyVaclav <vaclav.cernohorsky@gmail.com>
2019-10-14 21:41:05 +0200

    0.2.5
    Pridani par radku vis co

--- Unstable Atoms/Domain/Game/UnstableAtomsGame.swift
+++ Unstable Atoms/Domain/Game/UnstableAtomsGame.swift
@@ -20,6 +20,8 @@ class UnstableAtomsGame: UnstableAtoms {
     
     private (set) var columns: Int
     
+    private var blueTurn: Bool = true
+    
     init(rows: Int, columns: Int) {
         fields = Array(repeating: Array(repeating: Field(owner: .none, amountOfAtoms: 0), count: rows), count: columns)
         self.rows = rows
@@ -28,22 +30,35 @@ class UnstableAtomsGame: UnstableAtoms {
     
     // TODO: Implement
     func makeMove(coordinate: Coordinate) {
-        addAtoms(x: coordinate.x, y: coordinate.y)
+        var player: Player
+        if(blueTurn) {
+            player = Player.blue
+            blueTurn = false
+        } else {
+            player = Player.red
+            blueTurn = true
+        }
+        addAtoms(x: coordinate.x, y: coordinate.y, player: player)
         print("-----")
     }
     
-    func addAtoms(x: Int, y: Int) {
+    func addAtoms(x: Int, y: Int, player: Player) {
+        if(fields[x][y].owner == Player.blue && blueTurn == false) {
+            print("jednicka")
+        } else if(fields[x][y].owner == Player.red && blueTurn == true) {
+            print("dvojka")
+        }
         print("\(x) - \(y)")
         if(fields[x][y].amountOfAtoms == 0) {
-            fields[x][y] = Field(owner: Player.red, amountOfAtoms: 1)
+            fields[x][y] = Field(owner: player, amountOfAtoms: 1)
         } else if(fields[x][y].amountOfAtoms == 1) {
-            fields[x][y] = Field(owner: Player.red, amountOfAtoms: 2)
+            fields[x][y] = Field(owner: player, amountOfAtoms: 2)
         } else if(fields[x][y].amountOfAtoms == 2) {
-            fields[x][y] = Field(owner: Player.red, amountOfAtoms: 3)
+            fields[x][y] = Field(owner: player, amountOfAtoms: 3)
         } else if(fields[x][y].amountOfAtoms == 3) {
             //if(x < 5 && x > 0 && y < 8 && y > 0) {
-                fields[x][y] = Field(owner: Player.red, amountOfAtoms: 0)
-                divideAtoms(x: x, y: y)
+            fields[x][y] = Field(owner: Player.none, amountOfAtoms: 0)
+                divideAtoms(x: x, y: y, player: player)
             //} else {
             //    print("Outofrange")
             //}
@@ -52,19 +67,19 @@ class UnstableAtomsGame: UnstableAtoms {
         }
     }
     
-    func divideAtoms(x: Int, y: Int) {
+    func divideAtoms(x: Int, y: Int, player: Player) {
         
         if(x < 5) {
-            addAtoms(x: x + 1, y: y)
+            addAtoms(x: x + 1, y: y, player: player)
         }
         if(x > 0) {
-            addAtoms(x: x - 1, y: y)
+            addAtoms(x: x - 1, y: y, player: player)
         }
         if(y < 8) {
-            addAtoms(x: x, y: y + 1)
+            addAtoms(x: x, y: y + 1, player: player)
         }
         if(y > 0) {
-            addAtoms(x: x, y: y - 1)
+            addAtoms(x: x, y: y - 1, player: player)
         }
     }
 }
